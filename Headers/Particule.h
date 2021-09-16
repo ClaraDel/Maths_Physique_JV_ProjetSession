9f029fa6370ff4ff4577547410031fffb38ce5be
ClaraDel <clara.ferru@epfedu.fr>
2021-09-16 12:21:53 -0400

    mini ajout d'accesseurs pour l’attribut InverseMasse. Type à modifier

--- Headers/Particule.h
+++ Headers/Particule.h
@@ -5,11 +5,22 @@
 
 class Particule
 {
+    private :
+    int InverseMasse; // voir quel type mettre
+
     public:
-    
+
 	Particule operator*(float value) const;
 	void operator*=(float value);
+    int GetInverseMasse(); //voir quel type mettre
+
 	
 };
+
+// accesseurs pour l’attribut InverseMasse.
+int Particule::GetInverseMasse(){
+	return InverseMasse;
+
+}
 
 #endif
