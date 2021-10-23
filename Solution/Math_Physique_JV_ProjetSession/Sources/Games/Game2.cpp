2dfbb4fca6882a7eca67c74010da7a85e7cd084d
Camille Benoit <camille.benoit@mines-ales.org>
2021-10-23 17:52:55 -0400

    v

--- Solution/Math_Physique_JV_ProjetSession/Sources/Games/Game2.cpp
+++ Solution/Math_Physique_JV_ProjetSession/Sources/Games/Game2.cpp
@@ -128,7 +128,7 @@ void Game2::checkGroundCollisions(){
 			float penetration = radius - m_blob[i]->getPosition().getX();
 			Vecteur3D normal = Vecteur3D(1.0, 0.0, 0.0);
 			ParticleContact* particleContact = new ParticleContact(m_blob[i], NULL, 0.5, penetration, normal);
-			particuleContactList.push_back(particleContacst);
+			particuleContactList.push_back(particleContact);
 		}
 	}
 }
